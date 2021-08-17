128413d15b2722528e3e7bd854f435a59f603ddf
Gwonhyesoo <kysn22@naver.com>
2021-08-17 22:46:40 +0900

    10869

---
+++ 10869/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var A int
+	var B int
+
+	fmt.Scanf("%d %d", &A, &B)
+
+	fmt.Println(A + B)
+	fmt.Println(A - B)
+	fmt.Println(A * B)
+	fmt.Println(A / B)
+	fmt.Println(A % B)
+}
