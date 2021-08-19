d245b5f4d9824247e2622d5612ba1f1f4d5056c0
Gwonhyesoo <kysn22@naver.com>
2021-08-19 21:21:01 +0900

    1330 : 두 수 비교하기

--- 2588/main.go
+++ 2588/main.go
@@ -6,7 +6,8 @@ func main() {
 	var a int
 	var b int
 
-	fmt.Scanf("%d %d", &a, &b)
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
 	fmt.Println(a * ((b % 100) % 10))
 	fmt.Println(a * ((b % 100) / 10))
 	fmt.Println(a * (b / 100))
